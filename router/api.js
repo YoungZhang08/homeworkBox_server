5bb4712f3a0e53768637fa454379382aaa60f662
zhangyangyang <895114856@qq.com>
2019-01-23 12:07:47 +0800

    加入盒子

--- router/api.js
+++ router/api.js
@@ -1,6 +1,9 @@
 const router = require('koa-router')();
 const createBoxCtrl = require('../controller/ctrl_createbox');
+const addBoxCtrl = require('../controller/ctrl_addbox');
 
-const routers = router.post('/createBox', createBoxCtrl.createBox);
+const routers = router
+    .post('/createBox', createBoxCtrl.createBox)
+    .post('/addBox', addBoxCtrl.addBox);
 
 module.exports = routers;
