1f7e78c989bc0417d6838c05e70a285341a13fee
zhangyangyang <895114856@qq.com>
2019-01-30 10:39:00 +0800

    fix:完成发布作业接口

---
+++ controller/add_homework.js
@@ -0,0 +1,44 @@
+const { add, find } = require('../model/homework')
+
+const addHomework = async (ctx) => {
+  const homeworkInfo = ctx.request.body
+  console.log(ctx.request.body)
+  const { boxId, title, text, deadline } = homeworkInfo
+  if (!boxId || !title || !text || !deadline) {
+    return ctx.body = {
+      status: 400,
+      msg: '缺少参数',
+    }
+  }
+  await find(homeworkInfo).then(async res => {
+    if (res.length > 1) {
+      return ctx.body = {
+        status: 200,
+        msg: '欢迎登陆',
+      }
+    }
+
+    await add(homeworkInfo).then(() => {
+      return ctx.body = {
+        status: 200,
+        msg: '添加成功',
+      }
+    }, err => {
+      console.log(err, 3)
+      return ctx.body = {
+        status: 500,
+        msg: '添加出错',
+      }
+    })
+  }, err => {
+    console.log(err, 1)
+    return ctx.body = {
+      msg: '查询出错',
+      status: 400,
+    }
+  })
+}
+
+module.exports = {
+  addHomework,
+}
--- router/api.js
+++ router/api.js
@@ -3,11 +3,13 @@ const { createBox } = require('../controller/create_box')
 const { addUserBox } = require('../controller/add_user_box.js')
 const { addUser } = require('../controller/add_user')
 const { getRandom } = require('../controller/get_random')
+const { addHomework } = require('../controller/add_homework')
 
 const routers = router
   .post('/createBox', createBox)
-  .post('/addUSerBox', addUserBox)
+  .post('/addUserBox', addUserBox)
   .post('/addUser', addUser)
   .post('/getRandom', getRandom)
+  .post('/addHomework', addHomework)
 
 module.exports = routers
