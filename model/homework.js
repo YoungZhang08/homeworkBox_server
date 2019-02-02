486475655c8fe527caa8d4ff08a6d0a8c5a379be
zhangyangyang <895114856@qq.com>
2019-02-02 10:59:24 +0800

    为了拉代码。。。

--- model/homework.js
+++ model/homework.js
@@ -3,14 +3,14 @@ const mysql = require('../config/mysql.js')
 let add = async(data) => {
   let _sql = 'INSERT INTO homework(boxId, deadline, title, text) VALUES(?,?,?,?)'
   let res = await mysql.query(_sql, [data.boxId, data.deadline, data.title, data.text])
-  console.log(res)
+  // console.log(res)
   return res
 }
 
 let find = async(data) => {
   let _sql = 'SELECT * FROM homework WHERE boxId=?'
   let res = await mysql.query(_sql, [data.boxId])
-  console.log(res)
+  // console.log(res)
   return res
 }
 module.exports = {
