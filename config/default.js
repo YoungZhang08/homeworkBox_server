cff60d54d11a0ba76c4d8cba8db4b2bc17fab3a5
zhangyangyang <895114856@qq.com>
2019-05-06 10:24:59 +0800

    update

--- config/default.js
+++ config/default.js
@@ -3,13 +3,10 @@ const config = {
 
   database: {
     DATABASE: 'homework_box',
-    USERNAME: 'root',
-    PASSWORD: 'zyy960812',
-    // USERNAME: 'zhangchao',
-    // PASSWORD: 'zhangchao666',
+    USERNAME: 'zhangchao',
+    PASSWORD: 'zhangchao666',
     PORT: '3306',
-    HOST: 'localhost',
-    // HOST: '104.224.189.142',
+    HOST: '104.224.189.142',
   },
 }
 
